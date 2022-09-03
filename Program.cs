baf4c9794ee85c761b110e5eeccccb59c171a394
Egill Anton Hlöðversson <egill@payday.is>
2022-09-03 14:09:13 +0400

    Version 1.0.0

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JournalGenerator.Utils;
+
+Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("Applications");
+    Console.WriteLine("  1: Generate Journal from Islandsbanki Foreign Payments");
+    Console.WriteLine();
+    Console.WriteLine("Enter the number of application to run or press 'x' to exit: ");
+
+
+    string input = Console.ReadLine().ToLower();
+
+    if (input == "x")
+    {
+        break;
+    }
+
+    int applicationNr = -1;
+    if (!int.TryParse(input, out applicationNr))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Only input numbers ");
+        Console.ResetColor();
+        Console.ReadKey();
+    }
+    else
+    {
+        switch (applicationNr)
+        {
+            case 1:
+                GenerateJournalFromIslandsbankiForeignPayments();
+                break;
+            default:
+                Console.Write("Invalid applicaiton number");
+                Console.ResetColor();
+                Console.ReadKey();
+                break;
+        }
+    }
+}
+
+static void GenerateJournalFromIslandsbankiForeignPayments()
+{
+    string filePath;
+    FileInfo fileInfo;
+    while (true)
+    {
+        Console.WriteLine("Enter file path: ");
+        filePath = Console.ReadLine();
+        //filePath = @"C:\Users\egill\Desktop\Journal.xlsx"; // DEBUG
+        if (!File.Exists(filePath))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: File not found");
+            Console.ResetColor();
+            continue;
+        }
+
+        fileInfo = new FileInfo(filePath);
+
+        if (string.IsNullOrWhiteSpace(fileInfo.Extension) || !fileInfo.Extension.Equals(".xlsx", StringComparison.InvariantCultureIgnoreCase))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Error: File must be an Excel (.xlsx) file");
+            Console.ResetColor();
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    try
+    {
+        byte[] fileBytes = File.ReadAllBytes(filePath);
+        var outputFilePath = string.Format("{0}_ouput.xlsx", filePath.Replace(fileInfo.Extension, string.Empty));
+
+        (byte[] outputBytes, int journalEntriesCount) = PaydayJournalUtils.GenerateJournalFromIslandsbankiForeignPayments(fileBytes);
+
+        File.WriteAllBytes(outputFilePath, outputBytes);
+
+
+        Console.WriteLine(string.Format("Successfully generated {0} journal entries to file: {1}", journalEntriesCount, outputFilePath));
+        Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        string message = string.Format("{0} {1}", ex.Message, ex.InnerException != null ? ex.InnerException.Message : string.Empty);
+        Console.WriteLine(string.Format("Error: {0}", message));
+        Console.ResetColor();
+        Console.ReadLine();
+    }
+}
---
+++ Utils/PaydayJournalUtils.cs
@@ -0,0 +1,354 @@
+﻿using OfficeOpenXml;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace JournalGenerator.Utils
+{
+    internal class PaydayJournalUtils
+    {
+        internal static (byte[] outputBytes, int journalEntriesCount) GenerateJournalFromIslandsbankiForeignPayments(byte[] fileBytes)
+        {
+            string json = @"{
+	        'settings':[{
+		        'senderTitle':'AIRBNB PAYMENTS LUXEMBOURG S.A.',
+		        'value': 'Airbnb Experience',
+		        'commission': '0.2',
+		        'accountNumber': '2381'
+	        },
+	        {
+		        'senderTitle':'GetYourGuide Deutsch',
+		        'value': 'GetYourGuide',
+		        'commission': '0.3',
+		        'accountNumber': '2382'
+	        },
+	        {
+		        'senderTitle':'VIATOR LIMITED',
+		        'value': 'Viator',
+		        'commission': '0.255',
+		        'accountNumber': '2383'
+	        },
+	        {
+		        'senderTitle':'TRUST MY TRAVEL LIMITED',
+		        'value': 'Bókun',
+		        'commission': '0.015',
+		        'accountNumber': '2384'
+	        }]
+        }";
+
+            PaydayJournalSettings settings = JsonConvert.DeserializeObject<PaydayJournalSettings>(json);
+
+            List<PaydayJournalEntry> journalEntries = new();
+
+            // Import
+            using (MemoryStream memStream = new(fileBytes))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+
+                ExcelPackage excel = new(memStream);
+                for (int worksheetNr = 0; worksheetNr < excel.Workbook.Worksheets.Count; worksheetNr++)
+                {
+                    ExcelWorksheet worksheet = excel.Workbook.Worksheets[worksheetNr];
+                    int? postfix = null;
+                    bool isLast = false;
+
+                    for (int rowNumber = 2; rowNumber <= worksheet.Dimension.Rows; rowNumber++)
+                    {
+
+                        // Find start of next invoice
+                        string dateString = worksheet.GetValue<string>(rowNumber, 1);
+                        string payer = worksheet.GetValue<string>(rowNumber, 2);
+                        string reference = worksheet.GetValue<string>(rowNumber, 3);
+                        string currencySymbol = worksheet.GetValue<string>(rowNumber, 4);
+                        string sourceAmountString = worksheet.GetValue<string>(rowNumber, 5);
+                        string destinationAmountString = worksheet.GetValue<string>(rowNumber, 6);
+                        string status = worksheet.GetValue<string>(rowNumber, 7);
+
+
+                        if (string.IsNullOrWhiteSpace(dateString) || string.IsNullOrWhiteSpace(payer) || string.IsNullOrWhiteSpace(destinationAmountString))
+                        {
+                            break;
+                        }
+
+                        DateTime date = DateTime.FromOADate(long.Parse(dateString));
+
+                        int day = date.Day;
+                        int month = date.Month;
+                        int year = date.Year;
+
+                        decimal destinationAmount = decimal.Parse(destinationAmountString.Replace(".", ""), CultureInfo.InvariantCulture);
+
+                        PaydayJournalSetting paydayJournalSetting = settings.Settings.FirstOrDefault(x => x.SenderTitle.Equals(payer, StringComparison.InvariantCultureIgnoreCase));
+
+                        if (paydayJournalSetting == null)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Error: No settings found for {payer}");
+                            Console.ResetColor();
+                            continue;
+                        }
+
+                        // Check if we need to add a day apendix
+                        string nextLineDateString = worksheet.GetValue<string>(rowNumber + 1, 1);
+                        if (!string.IsNullOrWhiteSpace(nextLineDateString))
+                        {
+
+                            DateTime nextLineDate = DateTime.FromOADate(long.Parse(nextLineDateString));
+                            int nextLineDay = nextLineDate.Day;
+                            int nextLineMonth = nextLineDate.Month;
+                            int nextLineYear = nextLineDate.Year;
+                            string nextLinePayer = worksheet.GetValue<string>(rowNumber + 1, 2);
+                            PaydayJournalSetting nextLinePaydayJournalSetting = settings.Settings.FirstOrDefault(x => x.SenderTitle.Equals(nextLinePayer, StringComparison.InvariantCultureIgnoreCase));
+
+
+                            if (day == nextLineDay && month == nextLineMonth && year == nextLineYear && !string.IsNullOrWhiteSpace(paydayJournalSetting.Value) && paydayJournalSetting.Value == nextLinePaydayJournalSetting.Value)
+                            {
+                                postfix = postfix == null ? 1 : postfix + 1;
+                            }
+                            else if (postfix != null)
+                            {
+                                isLast = true;
+                                postfix = postfix + 1;
+                            }
+                            else
+                            {
+                                postfix = null;
+                            }
+                        }
+                        else
+                        {
+                            postfix = null;
+                        }
+
+                        string description = $"{paydayJournalSetting.Value} - {day}.{month}.{year}" + (postfix != null && postfix.HasValue ? $" - {postfix.Value.ToString()}" : "");
+
+                        if (isLast)
+                        {
+                            isLast = false;
+                            postfix = null;
+                        }
+
+
+                        decimal amount = Math.Round(destinationAmount, 0, MidpointRounding.AwayFromZero);
+                        decimal amountInclVat = Math.Round(paydayJournalSetting.Commission != 1 ? (destinationAmount / (1 - paydayJournalSetting.Commission)) : 0, MidpointRounding.AwayFromZero);
+                        decimal vat = amountInclVat - amount;
+                        decimal bankFee = 680M;
+
+                        // Add 5 entries for each row
+                        journalEntries.AddRange(new List<PaydayJournalEntry> {
+                            new PaydayJournalEntry
+                            {
+                                EntryNr = rowNumber - 1,
+                                Date = $"{day}/{month}/{year}",
+                                DateTimeValue = new DateTime(year, month, day),
+                                Description = description,
+                                Type = 1,
+                                Key = "1110",
+                                Amount = amountInclVat * -1, // Credit
+		                        VAT = "11",
+		                        //Reference = reference,
+		                        ReceiverKey = string.Empty
+                            },
+                            new PaydayJournalEntry
+                            {
+                                EntryNr = rowNumber - 1,
+                                Date = $"{day}/{month}/{year}",
+                                Description = description,
+                                Type = 1,
+                                Key = paydayJournalSetting.AccountNumber,
+                                Amount = vat,
+                                VAT = "",
+                                Reference = reference,
+                                ReceiverKey = string.Empty
+                            },
+                            new PaydayJournalEntry
+                            {
+                                EntryNr = rowNumber - 1,
+                                Date = $"{day}/{month}/{year}",
+                                Description = description,
+                                Type = 1,
+                                Key = "3200",
+                                Amount = amount,
+                                VAT = "",
+                                Reference = reference,
+                                ReceiverKey = string.Empty
+                            },
+                            new PaydayJournalEntry
+                            {
+                                EntryNr = rowNumber - 1,
+                                Date = $"{day}/{month}/{year}",
+                                Description = description,
+                                Type = 1,
+                                Key = "2990",
+                                Amount = bankFee,
+                                VAT = "",
+                                Reference = reference,
+                                ReceiverKey = string.Empty
+                            },
+                            new PaydayJournalEntry
+                            {
+                                EntryNr = rowNumber - 1,
+                                Date = $"{day}/{month}/{year}",
+                                Description = description,
+                                Type = 1,
+                                Key = "3200",
+                                Amount = bankFee * -1, // Credit
+		                        VAT = "",
+                                Reference = reference,
+                                ReceiverKey = string.Empty
+                            }
+                        });
+                    }
+                }
+            }
+
+            // Export
+            if (journalEntries.Count > 0)
+            {
+                journalEntries = (from je in journalEntries
+                                  orderby je.EntryNr descending
+                                  select je).ToList();
+            }
+
+            using MemoryStream stream = new();
+            using (ExcelPackage package = new(stream))
+            {
+                int rowNumber = 1;
+                int numberOfColumns = 9;
+
+                int sheetNr = 1;
+                int entryNr = 1;
+
+                foreach (var batch in journalEntries.Chunk(9*5)) // 9 Entries, each 5 lines
+                {
+                    var w = package.Workbook.Worksheets.Add($"Sheet{sheetNr}");
+                    sheetNr++;
+
+                    w.Cells[rowNumber, 1].Value = "Færsla nr.";
+                    w.Cells[rowNumber, 2].Value = "Dags.";
+                    w.Cells[rowNumber, 3].Value = "Lýsing";
+                    w.Cells[rowNumber, 4].Value = "Tegund (1 = Fjárhagur, 2 = Viðskiptavinur, 3 = Lánardrottinn)";
+                    w.Cells[rowNumber, 5].Value = "Lykill (fjárhagslykill eða kennitala)";
+                    w.Cells[rowNumber, 6].Value = "Fjárhæð m/vsk";
+                    w.Cells[rowNumber, 7].Value = "VSK (0, 11, 24) - tómt engin vsk";
+                    w.Cells[rowNumber, 8].Value = "Tilvísun";
+                    w.Cells[rowNumber, 9].Value = "Mótlykill";
+
+                    foreach (var subBatch in batch.Chunk(5))
+                    {
+                        foreach (var journalEntry in subBatch)
+                        {
+                            rowNumber++;
+                            w.Cells[rowNumber, 1].Value = entryNr;
+                            w.Cells[rowNumber, 2].Value = journalEntry.Date;
+                            w.Cells[rowNumber, 3].Value = journalEntry.Description;
+                            w.Cells[rowNumber, 4].Value = journalEntry.Type;
+                            w.Cells[rowNumber, 5].Value = journalEntry.Key;
+                            w.Cells[rowNumber, 6].Value = journalEntry.Amount;
+                            w.Cells[rowNumber, 7].Value = journalEntry.VAT;
+                            //w.Cells[rowNumber, 8].Value = journalEntry.Reference;
+                            w.Cells[rowNumber, 9].Value = journalEntry.ReceiverKey;
+                        }
+                        entryNr++;
+                    }
+                    w.Cells.AutoFitColumns(0); // Autofit columns for all cells
+                    rowNumber = 1; // reset
+                }
+
+                package.Workbook.Properties.Title = "Journal " + DateTime.Now.ToString("yyyyMMdd");
+                package.Save();
+            }
+            return (stream.ToArray(), journalEntries.Count / 5);
+        }
+    }
+
+    public partial class PaydayJournalEntry
+    {
+        public int EntryNr { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public int Type { get; set; }
+        public string Key { get; set; }
+        public Decimal Amount { get; set; }
+        public string VAT { get; set; }
+        public string Reference { get; set; }
+        public string ReceiverKey { get; set; }
+        public DateTime DateTimeValue { get; set; }
+    }
+
+
+    public partial class PaydayJournalSettings
+    {
+        [JsonProperty("settings")]
+        public List<PaydayJournalSetting> Settings { get; set; }
+    }
+
+    public partial class PaydayJournalSetting
+    {
+        [JsonProperty("senderTitle")]
+        public string SenderTitle { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("commission")]
+        public decimal Commission { get; set; }
+
+        [JsonProperty("accountNumber")]
+        public string AccountNumber { get; set; }
+    }
+
+    public partial class Temperatures
+    {
+        public static Temperatures FromJson(string json) => JsonConvert.DeserializeObject<Temperatures>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Temperatures self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+    }
+}
